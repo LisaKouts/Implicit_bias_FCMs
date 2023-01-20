c7504e4b66305f60c9c7d5c8ac9fe68bca96700a
Lisa_kouts <54538081+LisaKouts@users.noreply.github.com>
2023-01-20 14:25:48 +0100

    Delete bfcm directory

--- bfcm/bfcm/__init__.py
+++
@@ -1,1 +0,0 @@
-from .bfcm import reasoning
--- bfcm/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='bfcm',
-    version='0.0.2',
-    author='L. Koutsoviti, G. Napoles',
-    description='A library that quantifies implicit bias in tabular data with Fuzzy Cognitive Maps',
-    packages=['bfcm']
-)
