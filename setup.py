09ee2ef8d919cae7792a8c50bdb16d7b8e3a4cff
Lisa_kouts <54538081+LisaKouts@users.noreply.github.com>
2023-01-20 14:58:35 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='bfcm',
+    version='0.0.3',
+    author='L. Koutsoviti, G. Napoles',
+    description='A library that quantifies implicit bias in tabular data with Fuzzy Cognitive Maps',
+    long_description='This python package includes the functions related to paper "Modeling Implicit Bias with Fuzzy Cognitive Maps" by Gonzalo Nápoles, Isel Grau, Leonardo Concepción, Lisa Koutsoviti Koumeri and João Paulo Papa (see here https://doi.org/10.1016/j.neucom.2022.01.070).',
+    packages=['bfcm'],
+    license='MIT'
+)
